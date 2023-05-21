e3843c572a1a649c4454f578faf5b6289f785772
t-miyagahara <t-miyagahara@new-one.co.jp>
2023-05-21 10:27:46 +0900

    mv react redux default page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
-import { store } from "./app/store"
+import { store } from "./pages/react-redux/app/store"
 import App from "./App"
 import "./index.css"
 
